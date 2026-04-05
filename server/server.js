78073561f4c8c6b1c112962d469542d750a64f2b
Zinmin Aung <zinminaung@Zinmins-MacBook-Air.local>
2026-04-05 12:00:41 +0700

    fix server cros

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
 const socketHandler = require('./socket/socket')
+const cors = require('cors');
+
 require('dotenv').config();
 
-const port = process.env.PORT 
+const port = process.env.PORT || 3000;
 
 
 const app = express();
@@ -15,6 +17,14 @@ const io = new Server(server, {
         origin: process.env.CLIENT_URL
     }
 });
+
+// middlewares
+// CORS for HTTP routes
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
+
 
 
 app.get('/', (req, res) => {
@@ -27,6 +37,6 @@ socketHandler(io);
 
 
 
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
 })
